931f40525383f223b29ff2be490593cc2d3018ce
Gabriel Garcia <gabriel.garcia.ivan@outlook.com>
2022-07-05 20:00:00 -0300

    Navbar desafio 2

---
+++ src/components/NavBar.js
@@ -0,0 +1,20 @@
+const NavBar = () => {
+    return (  
+        <nav className="navbar navbar-expand bg-dark">
+            <div className="container-fluid">
+
+                <a className="navbar-brand text-warning" href="#">Click-World</a>
+                
+                <ul className="navbar-nav">
+                    <li><a className="nav-link text-light px-5" href="#">PRODUCTOS</a></li>
+                    <li><a className="nav-link text-light px-5" href="#">INFORMACION</a></li>
+                    <li><a className="nav-link text-light px-5" href="#">COMUNIDAD</a></li>
+                    <li><a className="nav-link text-light px-5" href="#">CONTACTO</a></li>
+                </ul>
+                
+            </div>
+        </nav>
+    );
+}
+ 
+export default NavBar;
